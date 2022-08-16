68b5b4b781de352c83694e6ae9b8b3b0ba94ab1b
Ayanda <68080779+mseshiAyanda@users.noreply.github.com>
2022-08-16 14:09:30 +0200

    Update settings.py

--- video_stream/settings.py
+++ video_stream/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
